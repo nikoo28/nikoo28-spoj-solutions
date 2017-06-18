40ff967fc6e4fc562aa65642aa7b36ec3c46726e
Nikhil Lohia <nikoo28@gmail.com>
2017-06-18 14:40:27 -0500

    Library Fine

---
+++ HACKERRANK/src/LibraryFine.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Created by nikoo28 on 6/18/17.
+ */
+public class LibraryFine {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int d1 = in.nextInt();
+    int m1 = in.nextInt();
+    int y1 = in.nextInt();
+    int d2 = in.nextInt();
+    int m2 = in.nextInt();
+    int y2 = in.nextInt();
+    if (y1 > y2)
+      System.out.println("10000");
+    else if (m1 > m2 && y1 == y2)
+      System.out.println((m1 - m2) * 500);
+    else if (d1 > d2 && m1 == m2 && y1 == y2)
+      System.out.println((d1 - d2) * 15);
+    else System.out.println("0");
+  }
+
+}
