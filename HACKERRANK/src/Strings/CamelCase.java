ad63cca226abcfba45ccf21b9ed2c1990f06744a
Nikhil Lohia <nikoo28@gmail.com>
2017-07-04 15:58:22 -0500

    Camel Case

---
+++ HACKERRANK/src/Strings/CamelCase.java
@@ -0,0 +1,15 @@
+package Strings;
+
+import java.util.Scanner;
+
+/**
+ * Created by nikoo28 on 7/4/17.
+ */
+public class CamelCase {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String s = in.next();
+    System.out.println(s.length() - s.replaceAll("[A-Z]", "").length() + 1);
+  }
+}
