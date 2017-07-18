0c1b5130f4160515467968ff585c55a5cf469dc3
Nikhil Lohia <nikoo28@gmail.com>
2017-07-17 19:06:07 -0500

    Uniform Weight Strings Gemstones

---
+++ HACKERRANK/src/Strings/Gemstones.java
@@ -0,0 +1,49 @@
+package Strings;
+
+import java.util.*;
+
+/**
+ * Created by nikhil.lohia on 07/17/2017.
+ */
+class Gemstones {
+
+  static int gemstones(String[] arr) {
+    // Complete this function
+
+    String str = arr[0];
+    Set<String> characterSet = new HashSet<>();
+    for (int i = 0; i < str.length(); i++) {
+      characterSet.add(String.valueOf(str.charAt(i)));
+    }
+
+    for (int i = 1; i < arr.length; i++) {
+
+      String x = arr[i];
+      Set<String> toRemove = new HashSet<>();
+      for (String character : characterSet) {
+        if (x.contains(character))
+          continue;
+        else
+          toRemove.add(character);
+      }
+
+      characterSet.removeAll(toRemove);
+      if (characterSet.isEmpty())
+        break;
+    }
+
+    return characterSet.size();
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    String[] arr = new String[n];
+    for (int arr_i = 0; arr_i < n; arr_i++) {
+      arr[arr_i] = in.next();
+    }
+    int result = gemstones(arr);
+    System.out.println(result);
+  }
+}
+
---
+++ HACKERRANK/src/Strings/UniformWeights.java
@@ -0,0 +1,41 @@
+package Strings;
+
+import java.util.*;
+
+/**
+ * Created by nikhil.lohia on 07/17/2017.
+ */
+public class UniformWeights {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String s = in.next();
+    int n = in.nextInt();
+
+    Set<Integer> sumList = new HashSet<>();
+    char prev = s.charAt(0);
+    sumList.add(prev - 'a' + 1);
+    int sum = prev - 'a' + 1;
+    for (int i = 1; i < s.length(); i++) {
+      char next = s.charAt(i);
+      if (next == prev) {
+        sum += (prev - 'a' + 1);
+        sumList.add(sum);
+      } else {
+        sumList.add(next - 'a' + 1);
+        prev = next;
+        sum = prev - 'a' + 1;
+      }
+    }
+
+    for (int a0 = 0; a0 < n; a0++) {
+      int x = in.nextInt();
+      // your code goes here
+      if (sumList.contains(x))
+        System.out.println("Yes");
+      else
+        System.out.println("No");
+    }
+  }
+}
+
