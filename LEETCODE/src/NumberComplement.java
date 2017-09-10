b4dc001d092e159616306ff626ca50ad4375ce95
Nikhil Lohia <nikoo28@gmail.com>
2017-09-10 14:26:30 -0700

    [LEETCODE] : Number complement

---
+++ LEETCODE/src/NumberComplement.java
@@ -0,0 +1,18 @@
+/**
+ * @author nikoo28 on 9/10/17
+ */
+public class NumberComplement {
+
+  public int findComplement(int num) {
+    int i = 0;
+    int j = 0;
+
+    while (i < num) {
+      i += Math.pow(2, j);
+      j++;
+    }
+
+    return i - num;
+  }
+
+}
