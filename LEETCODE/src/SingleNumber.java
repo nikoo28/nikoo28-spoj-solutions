0834d97482df4c08915bb06b8f5a844fb570e7b1
Nikhil Lohia <nikoo28@gmail.com>
2017-09-16 03:46:51 -0700

    Single Number

---
+++ LEETCODE/src/SingleNumber.java
@@ -0,0 +1,15 @@
+/**
+ * @author nikoo28 on 9/16/17
+ */
+public class SingleNumber {
+
+  public int singleNumber(int[] nums) {
+    int sing = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      sing = sing ^ nums[i];
+    }
+    return sing;
+
+  }
+
+}
