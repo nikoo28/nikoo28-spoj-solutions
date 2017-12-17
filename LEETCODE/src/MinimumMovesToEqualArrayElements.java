1d55150376ac2bed31eae87615523e167e162a68
Nikhil Lohia <nikoo28@gmail.com>
2017-12-17 14:44:39 -0700

    [LEETCODE] : Minimum moves to equal array elements

---
+++ LEETCODE/src/MinimumMovesToEqualArrayElements.java
@@ -0,0 +1,13 @@
+import java.util.stream.IntStream;
+
+/**
+ * Created by nikoo28 on 12/17/17 2:44 PM
+ */
+
+public class MinimumMovesToEqualArrayElements {
+
+  public int minMoves(int[] nums) {
+    return IntStream.of(nums).sum() - nums.length * IntStream.of(nums).min().getAsInt();
+  }
+
+}
