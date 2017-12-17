95ac1b8ffe557bdccadae3a609ba55ce399ff75c
Nikhil Lohia <nikoo28@gmail.com>
2017-12-17 14:58:35 -0700

    [LEETCODE] : Best time to buy and sell a stock 2

---
+++ LEETCODE/src/BestTimeToBuyAndSellAStockII.java
@@ -0,0 +1,28 @@
+/**
+ * Created by nikoo28 on 12/17/17 2:58 PM
+ */
+
+public class BestTimeToBuyAndSellAStockII {
+
+  public int maxProfit(int[] prices) {
+
+    if (prices.length < 2) return 0;
+
+    int buyingPrice = prices[0];
+    int profit = 0;
+
+    for (int i = 1; i < prices.length; i++) {
+
+      if (prices[i] < buyingPrice) {
+        buyingPrice = prices[i];
+        continue;
+      }
+
+      profit += prices[i] - buyingPrice;
+      buyingPrice = prices[i];
+    }
+
+    return profit;
+  }
+
+}
