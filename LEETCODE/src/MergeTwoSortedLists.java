7c36d366e48527f54ae046a2660ec8bf4ffb87cb
Nikhil Lohia <nikoo28@gmail.com>
2019-07-10 02:19:46 -0700

    Code refactor

--- LEETCODE/src/MergeTwoSortedLists.java
+++ LEETCODE/src/MergeTwoSortedLists.java
@@ -6,18 +6,39 @@ public class MergeTwoSortedLists {
 
   public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-    if (l1 == null) {
+    if (l1 == null)
       return l2;
-    } else if (l2 == null) {
+
+    if (l2 == null)
       return l1;
-    } else if (l1.val < l2.val) {
-      l1.next = mergeTwoLists(l1.next, l2);
-      return l1;
+
+    ListNode pointer = null;
+    ListNode answer = null;
+
+    if(l1.val < l2.val) {
+      answer = new ListNode(l1.val);
+      l1 = l1.next;
     } else {
-      l2.next = mergeTwoLists(l1, l2.next);
-      return l2;
+      answer = new ListNode(l2.val);
+      l2 = l2.next;
+    }
+    pointer = answer;
+
+    while (l1 != null && l2 != null) {
+
+      if (l1.val < l2.val) {
+        pointer.next = new ListNode(l1.val);
+        l1 = l1.next;
+      } else {
+        pointer.next = new ListNode(l2.val);
+        l2 = l2.next;
+      }
+      pointer = pointer.next;
     }
 
+    pointer.next = l1 == null ? l2 : l1;
+
+    return answer;
   }
 
 }
