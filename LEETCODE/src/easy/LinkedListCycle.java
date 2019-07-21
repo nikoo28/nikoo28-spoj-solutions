e8d0271cc3f08d1f19b58996e404fe15b12c0149
Nikhil Lohia <nikoo28@gmail.com>
2019-07-21 12:55:54 -0700

    LinkedList cycle

---
+++ LEETCODE/src/easy/LinkedListCycle.java
@@ -0,0 +1,27 @@
+package easy;
+
+/**
+ * Created by nikoo28 on 2019-07-21 12:55
+ */
+
+public class LinkedListCycle {
+
+  public boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null)
+      return false;
+    ListNode slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (slow == fast) {
+        while (head != slow) {
+          head = head.next;
+          slow = slow.next;
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+}
