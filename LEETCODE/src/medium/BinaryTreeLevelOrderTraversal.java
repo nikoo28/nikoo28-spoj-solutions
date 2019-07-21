c8252d49b8d966be54fd617b0cd13931e419c5c1
Nikhil Lohia <nikoo28@gmail.com>
2019-07-21 12:44:24 -0700

    Binary Tree Level Order Traversal

--- LEETCODE/src/medium/BinaryTreeLevelOrderTraversal.java
+++ LEETCODE/src/medium/BinaryTreeLevelOrderTraversal.java
@@ -1,3 +1,5 @@
+package medium;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
---
+++ LEETCODE/src/medium/TreeNode.java
@@ -0,0 +1,15 @@
+package medium;
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+}
